9b3adacbd283b8868ca039f27839a917cd67d828
Anton Rogov <anton@blackbits.pro>
2017-02-13 15:18:03 +0300

    adding support for path prefix

--- definitions/app.rb
+++ definitions/app.rb
@@ -1,15 +1,21 @@
-define :app, owner: nil, paths: [:default] do
+define :app, owner: nil,
+             path_prefix: nil,
+             paths: [:default] do
   name = params[:name]
   username = params[:owner]
   paths = Array params[:paths]
+  path_prefix = params[:path_prefix]
+  path_prefix = "/#{path_prefix}" if path_prefix && !path_prefix.start_with?('/')
+  shared_path = "shared#{path_prefix}"
 
   if paths.include? :default
     paths.delete :default
     paths += ['releases',
-              'shared',
-              'shared/config',
-              'shared/log',
-              'shared/tmp']
+              'shared']
+    paths << shared_path if path_prefix
+    paths += ["#{shared_path}/config",
+              "#{shared_path}/log",
+              "#{shared_path}/tmp"]
   end
 
   root = "#{node.app.path}/#{name}"
